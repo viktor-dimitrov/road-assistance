8bdd60e98910536462984b6c2091d51d9095cb9b
Viktor Dimitrov <106183268+viktor-dimitrov@users.noreply.github.com>
2024-03-03 18:36:56 +0200

    add more icons

--- client/richard-auto/src/components/Services/Services.jsx
+++ client/richard-auto/src/components/Services/Services.jsx
@@ -3,7 +3,40 @@ import styles from "./Services.module.css";
 export default function Services () {
 
     return(
-        <section>
+
+
+       
+    
+               <section>
+
+        
+            <article className={styles['service-card']} >
+                <div className={styles['icon']}>
+                <img src="src/assets/svg/day-and-night-icon.svg" alt="battery" />
+                </div>
+                <p>24/7</p>
+            </article>
+
+            <article className={styles['service-card']} >
+                <div className={styles['icon']}>
+                <img src="src/assets/svg/fast-speed-icon.svg" alt="battery" />
+                </div>
+                <p>Експресно пристигане</p>
+            </article>
+
+            <article className={styles['service-card']} >
+                <div className={styles['icon']}>
+                <img src="src/assets/svg/country-icon.svg" alt="battery" />
+                </div>
+                <p>Цяла Европа</p>
+            </article>
+
+            <article className={styles['service-card']} >
+                <div className={styles['icon']}>
+                <img src="src/assets/svg/roadside-car-assistance-icon.svg" alt="battery" />
+                </div>
+                <p>Репатриране</p>
+            </article>
 
             <article className={styles['service-card']} >
                 <div className={styles['icon']}>
@@ -27,6 +60,9 @@ export default function Services () {
             </article>
 
 
+
         </section>
+      
+
     )
 }
