a48b66826364d6170b40eea6db51d96d2bd7751f
Viktor Dimitrov <106183268+viktor-dimitrov@users.noreply.github.com>
2024-03-17 19:16:00 +0200

    css updates

--- client/richard-auto/src/components/Home/Home.jsx
+++ client/richard-auto/src/components/Home/Home.jsx
@@ -7,23 +7,29 @@ export default function Home() {
 
         <main>
 
-          <div className={styles['title']} >
+        <div className={styles['background']}>
+          <img className={styles['background-img']} src="/images/bg3.png" alt="road" />
+        </div>
 
 
-            <h1 > <strong>24/7</strong> <strong> ПЪТНА ПОМОЩ  </strong> </h1>
-            <button className={styles['callme']}><img src="/svg/24-hours-phone-support-icon.svg" alt="24/7" />  <p>0899633333</p></button>
-           
-
-          
-          </div>
-
-          <div className={styles['img']} >
-            <div className={styles['shadow']}></div>
-            <img src="/images/main.png" alt="road-assistance" />
-          </div>
+        <div className={styles['title']} >
 
 
-        </main>
+          <h1 > <strong>24/7</strong> <strong> ПЪТНА ПОМОЩ  </strong> </h1>
+          <button className={styles['callme']}><img src="/svg/24-hours-phone-support-icon.svg" alt="24/7" />  <p>0899633333</p></button>
+
+
+
+        </div>
+
+
+        <div className={styles['main-img']} >
+
+          <img src="/images/main.png" alt="road-assistance" />
+        </div>
+
+
+      </main>
 
    
     )
